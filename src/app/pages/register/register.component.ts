39676fab7fe4cde656cdfeec1466d84011c4fb37
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-16 16:54:50 +0100

    👔 Implement form validation logic in Register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,4 +9,28 @@ import { CommonModule } from '@angular/common';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export default class RegisterComponent {}
+export default class RegisterComponent implements OnInit {
+  fb = inject(FormBuilder);
+
+  registerForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(4)]),
+      ],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(8)]),
+      ],
+      confirmPassword: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(8)]),
+      ],
+    });
+  }
+}
