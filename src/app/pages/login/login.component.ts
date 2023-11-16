ef11cebbd97d4778e64e55619d70d5eeed3697b5
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-16 23:05:32 +0100

    ✨ Create Login component with form and validation
    - Included form fields for email and password with required validations.
    - Set up basic form handling logic in the LoginComponent class.
    - Prepared UI for the login form with validation feedback.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  loginForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
+}
