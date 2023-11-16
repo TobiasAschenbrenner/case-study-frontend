77fe1432eec223fb3bb800ecc7a04e888aaa0b9c
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-16 19:50:15 +0100

    ✨ Implement AuthService for user registration

---
+++ src/app/config.ts
@@ -0,0 +1,3 @@
+export const API_BASE_URL = {
+  authServiceApi: 'http://localhost:8800/api/auth/',
+};
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { API_BASE_URL } from '../config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  http = inject(HttpClient);
+
+  registerService(registerObj: any) {
+    return this.http.post<any>(
+      `${API_BASE_URL.authServiceApi}register`,
+      registerObj
+    );
+  }
+}
