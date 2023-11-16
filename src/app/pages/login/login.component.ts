bb3b757329c725eeb814953cae68bd1501b3b3c9
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-16 23:09:56 +0100

    🎨 Add navigation links between Login and Register pages

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,13 +6,13 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
