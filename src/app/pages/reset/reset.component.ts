82364b8c2d1700db5bb7ed00232543dbafd1d7c3
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-17 17:35:55 +0100

    ✨ Create Reset Password component with token validation
    - Developed ResetComponent with a form for password resetting.
    - Added password and confirm password fields.
    - Implemented token retrieval from route parameters for password reset validation.
    - Set up a basic UI layout for the Reset Password form.

--- src/app/pages/reset/reset.component.ts
+++ src/app/pages/reset/reset.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { confirmPasswordValidator } from './../../validators/confirm-password.validator';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './reset.component.html',
   styleUrls: ['./reset.component.scss'],
 })
-export default class ResetComponent {}
+export default class ResetComponent implements OnInit {
+  resetForm!: FormGroup;
+  fb = inject(FormBuilder);
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
+  token!: string;
+
+  ngOnInit(): void {
+    this.resetForm = this.fb.group(
+      {
+        password: [
+          '',
+          Validators.compose([Validators.required, Validators.minLength(8)]),
+        ],
+        confirmPassword: [
+          '',
+          Validators.compose([Validators.required, Validators.minLength(8)]),
+        ],
+      },
+      {
+        validator: confirmPasswordValidator('password', 'confirmPassword'),
+      }
+    );
+
+    this.activatedRoute.params.subscribe((val) => {
+      this.token = val['token'];
+      console.log(this.token);
+    });
+  }
+
+  reset() {
+    console.log(this.resetForm.value);
+  }
+}
