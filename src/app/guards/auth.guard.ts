bc0943c0c5832d7a97555e9dd454c4a2a75912d7
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-19 17:15:20 +0100

    ♻️ Enhance AuthGuard with Reactive Authentication Checks
    - Refactored the canActivate method in AuthGuard to return an Observable<boolean>.
    - Integrated the AuthService's isLoggedIn$ observable using RxJS operators for reactive authentication status handling.
    - Replaced the direct boolean check with a map operator to streamline the route access decision based on the user's login status.
    - Implemented navigation to the login page within the observable pipeline for users who are not authenticated.
    - Improved overall asynchronous handling in AuthGuard, aligning it more closely with Angular's reactive and asynchronous patterns.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, CanActivate } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard {
+export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.authService.hasValidSession()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
+  canActivate(): Observable<boolean> {
+    return this.authService.isLoggedIn$.pipe(
+      map((isLoggedIn) => {
+        if (!isLoggedIn) {
+          this.router.navigate(['/login']);
+          return false;
+        }
+        return true;
+      })
+    );
   }
 }
