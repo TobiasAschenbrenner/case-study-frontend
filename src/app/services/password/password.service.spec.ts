f0f1b13354b9aeb6a61a679181f23582e6a9b74d
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-20 16:42:37 +0100

    ✅ Add unit tests for PasswordService
    - Utilize HttpClientTestingModule and HttpTestingController to mock and handle HTTP requests.
    - Validate the correct behavior of resetPassword with expected POST request and response structure.
    - Ensure the response matches the mock response: {"success":true, "status":200, "message":"Password updated successfully!"}.
    - Confirm proper test setup and teardown with TestBed configuration and HttpTestingController verification.

--- src/app/services/password/password.service.spec.ts
+++ src/app/services/password/password.service.spec.ts
@@ -1,16 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PasswordService } from './password.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { API_BASE_URL } from '../../app.config';
 
 describe('PasswordService', () => {
   let service: PasswordService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PasswordService],
+    });
     service = TestBed.inject(PasswordService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should send a password reset request', () => {
+    const mockResetObj = { token: 'mock-token', password: 'newPassword123' };
+    const mockResponse = {
+      success: true,
+      status: 200,
+      message: 'Password updated successfully!',
+    };
+
+    service.resetPassword(mockResetObj).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpController.expectOne(
+      `${API_BASE_URL.authServiceApi}auth/reset-password`
+    );
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(mockResetObj);
+    req.flush(mockResponse);
   });
 });
