5ba89565cfc73698b61ea607968ea1c929f08e8f
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-20 15:56:25 +0100

    ✅ Add Tests for HeaderComponent
    - Include tests to verify the component's creation, reflection of login status based on AuthService's `isLoggedIn$` observable, and logout functionality.
    - Utilize BehaviorSubject to simulate AuthService's `isLoggedIn$` observable.
    - Ensure proper initialization and destruction of subscriptions in the component lifecycle.

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,15 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
+  let isLoggedInSubject: BehaviorSubject<boolean>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+    isLoggedInSubject = new BehaviorSubject<boolean>(false);
+    mockAuthService = jasmine.createSpyObj('AuthService', ['logout']);
+    Object.defineProperty(mockAuthService, 'isLoggedIn$', {
+      get: () => isLoggedInSubject.asObservable(),
     });
+
+    TestBed.configureTestingModule({
+      imports: [HeaderComponent, RouterTestingModule],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +29,20 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should reflect the login status based on AuthService', () => {
+    isLoggedInSubject.next(true);
+    fixture.detectChanges();
+    expect(component.isLoggedIn).toBeTrue();
+
+    isLoggedInSubject.next(false);
+    fixture.detectChanges();
+    expect(component.isLoggedIn).toBeFalse();
+  });
+
+  it('should call AuthService logout method on logout', () => {
+    component.logout();
+    expect(mockAuthService.logout).toHaveBeenCalled();
   });
 });
