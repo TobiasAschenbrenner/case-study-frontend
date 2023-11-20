2ddf3302e2a8a5859ce9be559e2d97accfdc7026
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-20 16:52:11 +0100

    ✅ Add Unit Tests for UserService
    - Implement tests for getUserById method.
    - Implement tests for getAllUsers method.

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,16 +1,86 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { API_BASE_URL } from '../../app.config';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService],
+    });
     service = TestBed.inject(UserService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve a user by ID', () => {
+    const mockUser = {
+      data: {
+        _id: '123',
+        firstName: 'John',
+        lastName: 'Doe',
+        username: 'johndoe',
+        email: 'john@example.com',
+        profilePicture: 'john.jpg',
+        isAdmin: false,
+      },
+    };
+
+    service.getUserById('123').subscribe((user) => {
+      expect(user).toEqual(mockUser);
+    });
+
+    const req = httpController.expectOne(
+      `${API_BASE_URL.authServiceApi}user/123`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUser);
+  });
+
+  it('should retrieve all users', () => {
+    const mockUsers = {
+      data: {
+        0: {
+          _id: '123',
+          firstName: 'John',
+          lastName: 'Doe',
+          username: 'johndoe',
+          email: 'john@example.com',
+          profilePicture: 'john.jpg',
+          isAdmin: false,
+        },
+        1: {
+          _id: '124',
+          firstName: 'Jane',
+          lastName: 'Doe',
+          username: 'janedoe',
+          email: 'jane@example.com',
+          profilePicture: 'jane.jpg',
+          isAdmin: true,
+        },
+      },
+    };
+
+    service.getAllUsers().subscribe((users) => {
+      expect(users).toEqual(mockUsers);
+    });
+
+    const req = httpController.expectOne(`${API_BASE_URL.authServiceApi}user`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers);
   });
 });
