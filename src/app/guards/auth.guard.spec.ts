4480ba845bf58a5249c81e26118febf606a50106
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-20 17:06:03 +0100

    ✅ Add tests for AuthGuard functionality
    - Implemented unit tests for AuthGuard in 'auth.guard.spec.ts'.
    - Tests cover the canActivate method's behavior based on user authentication status.
    - Includes scenarios for both authenticated and unauthenticated users.
    - Validates correct route navigation to the login page for unauthenticated users.

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -2,20 +2,51 @@ import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 import AuthGuard from './auth.guard';
+import { BehaviorSubject } from 'rxjs';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
-  let authService: AuthService;
-  let router: Router;
+  let authServiceMock: jasmine.SpyObj<AuthService>;
+  let routerMock: jasmine.SpyObj<Router>;
+  let isLoggedInSubject: BehaviorSubject<boolean>;
 
   beforeEach(() => {
-    authService = TestBed.inject(AuthService);
-    router = TestBed.inject(Router);
-    TestBed.configureTestingModule({});
-    guard = new AuthGuard(authService, router);
+    isLoggedInSubject = new BehaviorSubject<boolean>(false);
+    authServiceMock = jasmine.createSpyObj('AuthService', [], {
+      isLoggedIn$: isLoggedInSubject.asObservable(),
+    });
+    routerMock = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: routerMock },
+        AuthGuard,
+      ],
+    });
+
+    guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should allow the authenticated user to access app routes', () => {
+    isLoggedInSubject.next(true);
+
+    guard.canActivate().subscribe((isAllowed) => {
+      expect(isAllowed).toBeTrue();
+      expect(routerMock.navigate).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should redirect an unauthenticated user to the login page', () => {
+    isLoggedInSubject.next(false);
+
+    guard.canActivate().subscribe((isAllowed) => {
+      expect(isAllowed).toBeFalse();
+      expect(routerMock.navigate).toHaveBeenCalledWith(['/login']);
+    });
   });
 });
