2d0d7c27de81ada9edbb83446bcd795fe534a748
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-20 14:37:33 +0100

    ✅ Add Unit Tests for LoginComponent
    - Implementing TestBed setup to integrate ReactiveFormsModule and RouterTestingModule.
    - Creating mock objects for AuthService and injecting Router to handle navigation.
    - Testing form field definitions and validations.
    - Verifying the interaction between LoginComponent and AuthService.
    - Confirming that successful login leads to navigation to the 'home' route.
    - Adding error handling test.

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,15 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import LoginComponent from './login.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+
+interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface AuthResponse {
+  success: boolean;
+  data: User;
+  token: string;
+}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
+    mockAuthService = jasmine.createSpyObj('AuthService', ['login']);
+
     TestBed.configureTestingModule({
-      imports: [LoginComponent],
-    });
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([]), // No need for DummyHomeComponent now
+      ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        FormBuilder,
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +49,64 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the login form', () => {
+    expect(component.loginForm).toBeDefined();
+    expect(component.loginForm.controls['email']).toBeDefined();
+    expect(component.loginForm.controls['password']).toBeDefined();
+  });
+
+  it('should call AuthService login on form submit and navigate on success', () => {
+    const router = TestBed.inject(Router); // Inject the router
+    spyOn(router, 'navigate');
+
+    const mockAuthResponse: AuthResponse = {
+      success: true,
+      data: {
+        _id: '123',
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@example.com',
+        password: 'password',
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      token: 'mock-token',
+    };
+
+    mockAuthService.login.and.returnValue(of(mockAuthResponse));
+
+    component.loginForm.setValue({
+      email: 'test@example.com',
+      password: 'password',
+    });
+    component.login();
+
+    expect(mockAuthService.login).toHaveBeenCalledWith({
+      email: 'test@example.com',
+      password: 'password',
+    });
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+  });
+
+  it('should handle login error', () => {
+    mockAuthService.login.and.returnValue(
+      throwError(() => new Error('Login failed'))
+    );
+    spyOn(console, 'log');
+
+    component.loginForm.setValue({
+      email: 'test@example.com',
+      password: 'password',
+    });
+    component.login();
+
+    expect(mockAuthService.login).toHaveBeenCalledWith({
+      email: 'test@example.com',
+      password: 'password',
+    });
+    expect(console.log).toHaveBeenCalledWith('Failed to login');
   });
 });
